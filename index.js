bf238739097731df8d3d0911ab76f99d05cf8c1f
Kerrion Siokwu <ladykerrion@sky.com>
2026-01-30 15:11:14 +0000

    add README text

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import pool from "./db/db.js";
 import expressEjsLayouts from "express-ejs-layouts";
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(expressEjsLayouts);
@@ -248,6 +248,6 @@ app.post("/delete", async(req, res) => {
   
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
